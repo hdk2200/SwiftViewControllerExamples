3510d8136fd453460461814a4b799484bfd4c616
bj1024 <16552133+bj1024@users.noreply.github.com>
2019-06-07 09:52:17 +0900

    add NavigationWithBanner

---
+++ NavigationWithBanner/NavigationWithBanner/ContainerViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ContainerViewController.swift
+//  NavigationControllerWIthBanner
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
