7bba9ae6c94d27e0b19411bab8bfe44c599e4ff6
bj1024 <16552133+bj1024@users.noreply.github.com>
2020-07-13 19:24:18 +0900

    Add init deinit message for check memory deallocation.

--- NavigationControllerBasic/NavigationControllerBasic/FirstViewController.swift
+++ NavigationControllerBasic/NavigationControllerBasic/FirstViewController.swift
@@ -1,8 +1,14 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+
+
+  deinit {
+    print("deinit \(String(describing: type(of: self)))")
+  }
   override func viewDidLoad() {
     super.viewDidLoad()
+    print("viewDidLoad \(String(describing: type(of: self)))")
   }
 
   @IBAction func unwindActionFirstVC(unwindSegue _: UIStoryboardSegue) {}
--- NavigationControllerBasic/NavigationControllerBasic/SecondViewController.swift
+++ NavigationControllerBasic/NavigationControllerBasic/SecondViewController.swift
@@ -1,8 +1,13 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+
+  deinit {
+     print("deinit \(String(describing: type(of: self)))")
+   }
   override func viewDidLoad() {
     super.viewDidLoad()
+    print("viewDidLoad \(String(describing: type(of: self)))")
   }
 
   @IBAction func unwindActionSecondVC(unwindSegue _: UIStoryboardSegue) {}
